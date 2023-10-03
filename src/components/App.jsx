20795cf723032288d3910c7e8759568dd185aa0c
Daryna Holikova <dgolikova0@gmail.com>
2023-10-03 22:00:27 +0100

    redoing app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,72 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./SearchFilter/SearchFilter";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import {MainWrap,  Title} from './App.styled'
+const LS_KEY = 'contacts'
 
-export class App extends Component{
-  state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
-    filter: ''
-  }
+export const App =()=>{
 
-  handleAddContacts = (newContact) => {
-   const hasContactDuplicate = this.state.contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
+  const [contacts, setContacts] = useState([{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},])
+
+  const [filter, setFilter] = useState('')
+  const [filteredContactsList, setFilteredContactsList] = useState([]);
+
+  useEffect(()=>{
+    const contactsData = localStorage.getItem(LS_KEY)
+    const parsedContacts = JSON.parse(contactsData)
+    if(parsedContacts){
+       setContacts(parsedContacts)
+         }
+  }, [])
+
+
+    useEffect(()=>{
+      localStorage.setItem(LS_KEY, JSON.stringify(contacts))
+    }, [contacts])
+
+    useEffect(() => {
+      const normalizedFilter = filter.toLowerCase();
+      const filtered = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
+      setFilteredContactsList(filtered);
+    }, [contacts, filter]);
+
+  const handleAddContacts = (newContact) => {
+   const hasContactDuplicate = contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())
    if(hasContactDuplicate){
     Notify.failure(`${newContact.name} is already in your contacts`)
     return
    }
-   this.setState((prevState) => ({
-    contacts: [...prevState.contacts, newContact]
-   }))
+   setContacts((prevState) => [...prevState, newContact])
   }
 
-  handleDeleteContacts = (contactId) => {
-    this.setState(prevState => {
-      return{
-        contacts: prevState.contacts.filter((contact) => contact.id !== contactId)
-      }
-    })
-  }
+  const handleDeleteContacts = (contactId) => {
 
-  handleFilterInputChange = (e) => {
-    this.setState({
-      filter: e.target.value
-    })
-  }
+      setContacts((prevState) => prevState.filter((contact) => contact.id !== contactId));
+    };
+    
+  
 
-  handleFilter = () =>{
-    const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
+  const handleFilterInputChange = (e) => {
+    const value = e.target.value;
+    setFilter(value);
   }
+  
+  console.log(filter)
 
-  render(){
-    const filteredContacts = this.handleFilter()
     return(
       <MainWrap>
         <Title>Phonebook</Title>
-        <ContactForm handleAddContacts={this.handleAddContacts}/>
-        <Filter value={this.state.filter} onChange={this.handleFilterInputChange}/>
+        <ContactForm handleAddContacts={handleAddContacts}/>
+        <Filter value={filter} onChange={handleFilterInputChange}/>
 
         <Title>Contacts</Title>
-        <ContactList contacts={filteredContacts} handleDeleteContacts={this.handleDeleteContacts}/>
+        <ContactList contacts={filteredContactsList} handleDeleteContacts={handleDeleteContacts}/>
       </MainWrap>
     )
-  }
-}
+    }
+
